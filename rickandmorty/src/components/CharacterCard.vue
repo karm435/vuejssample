f22e996764d5e7e35e5f44b38e4302624ca4fd70
Karmjit Singh <karmjit.singh@purple.telstra.com>
2021-07-12 13:59:12 +1000

    Added styles for character cards

--- rickandmorty/src/components/CharacterCard.vue
+++ rickandmorty/src/components/CharacterCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="character-card">
-    <img :src="character.image">
-    <div class="name">
-      <a @click="characterClick(character.id)">{{character.name}}</a>
+    <img  class="character-image" :src="character.image">
+    <div class="character-details">
+      <a @click="characterClick(character.id)" class="character-name">{{character.name}}</a>
+      <div class="species"><span class="heading">Species:</span> {{character.species}}</div>
+      <div class="gender"><span class="heading">Gender: </span>{{character.gender}}</div>
     </div>
-    <div class="species">{{character.species}}</div>
-    <div class="gender">{{character.gender}}</div>
   </div>
 </template>
 
@@ -26,7 +26,30 @@ export default {
 
 <style scoped>
 .character-card {
-  font-size: 2em;
+  font-size: 1.5em;
+  display: grid;
+  border-radius: 7px;
+  box-shadow: 0 15px 20px rgba(0, 0, 0, 0.356);
+  margin: 7px;
+}
+
+.character-image {
+  width: 100%;
+  height: 100%;
+  border-radius: 7px 7px 0 0;
+}
+
+.character-details {
+  padding-left: 7px;
+  text-align: left;
+}
+
+.character-name {
+  font-weight: bold;
+}
+
+.heading {
+  color: gray;
 }
 
 a:hover{
